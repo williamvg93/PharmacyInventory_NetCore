0f62162680596fa8dfb944bc18649752c3d0981e
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-10-25 12:41:47 +0000

    feat: :sparkles: Program, Add Extensions to Program

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Api.Extensions;
+using AspNetCoreRateLimit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/* Add AddAPlicationServices */
+builder.Services.AddAplicationServices();
+
+/* Add Cors */
+builder.Services.ConfigureCors();
+
+/* Add Config RAte Limiting */
+builder.Services.ConfigureRatelimiting();
 
 var app = builder.Build();
 
@@ -17,6 +29,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+/* Use Cors */
+app.UseCors("CorsPolicy");
+
+/* Use RateLimiting */
+app.UseIpRateLimiting();
 
 app.UseAuthorization();
 
